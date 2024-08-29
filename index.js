27350a55cdaa73f7ae830e3c8bfcfa313de307c8
MARQUITOSX <116809028+MARQUITOSX@users.noreply.github.com>
2024-08-29 01:30:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let anoAtual = 2024;
+const nomeCompleto = "Marcos Vinicius";
+let idade = 17;
+const cursoFaculdade = "Ciências da Computação";
+let cidadeAtual = "SJP";
+
+console.log(`Olá, eu sou o ${nomeCompleto}, eu tenho ${idade} anos, moro em ${cidadeAtual} , quero ano que vem estar cursando ${cursoFaculdade}`);
+
+anoAtual = 2025;
+idade = 18;
+cidadeAtual = "BC"
+console.log(`Para meu ano que vem, meu nome ainda será ${nomeCompleto}, minha idade vai ser ${idade} anos, pretendo estar morando em ${cidadeAtual} e espero cursar ${cursoFaculdade}. `);
